368e151df41215b24d52b7417fa87a22617ea3c5
Sarah Kinneer <kinneers@gmail.com>
2018-12-16 15:58:31 -0500

    MVP with most css complete

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -15,16 +15,72 @@ var questions = [
     question: "Which animal poops in cubes?",
     number: "1",
     correctAnswer: "Wombat",
-    distractors: ["Ferret", "Platypus", "Wombat", "Quokka"],  //Be sure to make these display in random order
+    distractors: ["Ferret", "Platypus", "Wombat", "Quokka"],
     image: '<iframe src="https://giphy.com/embed/eW7AXuZ4w2sk8" width="480" height="384" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/funny-chewing-wombat-eW7AXuZ4w2sk8">via GIPHY</a></p>'
     },
     {
-    question: "This is the second question",
+    question: "Where does a shrimp keep its heart?",
     number: "2",
-    correctAnswer: "Second Correct Answer",
-    distractors: ["Second Correct Answer", "Distractor 1", "Distractor 2", "Distractor 3"],
-    image: ""
-    }
+    correctAnswer: "In its head",
+    distractors: ["In its head", "In its stomach", "In its partner", "In its tail"],
+    image: '<iframe src="https://giphy.com/embed/9QgdMczZy2u1q" width="480" height="351" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/forrest-gump-movie-9QgdMczZy2u1q">via GIPHY</a></p>'
+    },
+    {
+    question: "How many noses do slugs have?",
+    number: "3",
+    correctAnswer: "Four",
+    distractors: ["One", "Four", "Eight", "Ten"],
+    image: '<iframe src="https://giphy.com/embed/lma5aujULEkKs" width="480" height="351" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/lma5aujULEkKs">via GIPHY</a></p>'
+    },
+    {
+    question: "Which of these animals are the deadliest?",
+    number: "4",
+    correctAnswer: "Mosquitos",
+    distractors: ["Sharks", "Mosquitos", "Lions", "Dogs"],
+    image: '<iframe src="https://giphy.com/embed/8LfaJlBxiSq2c" width="480" height="367" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/1995-mosquito-8LfaJlBxiSq2c">via GIPHY</a></p>'
+    },
+    {
+    question: "What is a rhinoceros' horn made of?",
+    number: "5",
+    correctAnswer: "Hair",
+    distractors: ["Hair", "Cartilage", "Skin", "Bone"],
+    image: '<iframe src="https://giphy.com/embed/2wN5OlLBhBzc4" width="480" height="268" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/rhinoceros-2wN5OlLBhBzc4">via GIPHY</a></p>'
+    },
+    {
+    question: "What is the longest amount of time a snail can sleep?",
+    number: "6",
+    correctAnswer: "3 Years",
+    distractors: ["3 Hours", "3 Days", "3 Months", "3 Years"],
+    image: '<iframe src="https://giphy.com/embed/iQ6yGuMhPGWhW" width="480" height="257" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/nicki-minaj-anaconda-iQ6yGuMhPGWhW">via GIPHY</a></p>'
+    },
+    {
+    question: "What animal's fingerprints look so close to human fingerprints that they have been confused at crime scenes?",
+    number: "7",
+    correctAnswer: "Koalas",
+    distractors: ["Gorillas", "Red Pandas", "Koalas", "Chimpanzees"],
+    image: '<iframe src="https://giphy.com/embed/IWy60FY7KKOOc" width="480" height="354" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/koala-walking-IWy60FY7KKOOc">via GIPHY</a></p>'
+    },
+    {
+    question: "How far can cats jump?",
+    number: "8",
+    correctAnswer: "7 times their tail length",
+    distractors: ["3 times their tail length", "5 times their tail length", "7 times their tail length", "11 times their tail length"],
+    image: '<iframe src="https://giphy.com/embed/6n8kX73OlwSIM" width="480" height="270" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/cat-running-6n8kX73OlwSIM">via GIPHY</a></p>'
+    },
+    {
+    question: "What is the closest living animal to the Tyrannosaurus Rex?",
+    number: "9",
+    correctAnswer: "Chickens",
+    distractors: ["Chickens", "Crocodiles", "Eagles", "Komodo Dragons"],
+    image: '<iframe src="https://giphy.com/embed/Zg7clvqHE3CdW" width="480" height="269" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/chicken-spinning-disco-Zg7clvqHE3CdW">via GIPHY</a></p>'
+    },
+    {
+    question: "How do blue jays keep other birds away?",
+    number: "10",
+    correctAnswer: "They mimic hawk calls",
+    distractors: ["They beat them with their wings", "They fly in circles", "They pick up rocks in their beaks and throw them", "They mimic hawk calls"],
+    image: '<iframe src="https://giphy.com/embed/ogQzWZFbejR28" width="480" height="229" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/heart-group-theory-ogQzWZFbejR28">via GIPHY</a></p>'
+    },
 ];
 
 $(document).ready(function() {
@@ -32,12 +88,12 @@ $(document).ready(function() {
         $('#restart').hide();
         //Writes the question and answer choice buttons to the page
         function populateQuestion() {
-            $('#mainContent').text("Question " + questions[questionCount].number);
+            $('.questionStem').text("Question " + questions[questionCount].number);
             var stem = $('<p>');
             stem.text(questions[questionCount].question);
-            $('#mainContent').append(stem);
+            $('.questionStem').append(stem);
             for (var i = 0; i < questions[questionCount].distractors.length; i++) {
-                $('#mainContent').append('<button class="distractors">' + questions[questionCount].distractors[i] + '</button>');
+                $('.questionStem').append('<button class="distractors">' + questions[questionCount].distractors[i] + '</button>');
             }
 
             $('.distractors').on('click tap', function() {
@@ -54,19 +110,19 @@ $(document).ready(function() {
             //This function shows the correct answer after each question has been answered or displayed for 20 seconds
             function checkAnswer(answer) {
                     clearInterval(gameInterval);
-                    $('#mainContent').empty();
+                    $('.questionStem').empty();
                     //Show picture
-                    $('#mainContent')
+                    $('.questionStem')
                         .append(
                             `<div class="answerDiv">The correct answer is: ${answer}</div>
                             ${questions[questionCount].image}`
                     )
                     if (questionCount + 1 === questions.length) {
                         setTimeout(function(){
-                            $('#mainContent').empty();
-                            $('#mainContent')
+                            $('.questionStem').empty();
+                            $('.questionStem')
                                 .append(`<p>Thanks for playing!</p>
-                                        <p>Your Score: </p>
+                                        <p>Your Score: ${(correct/(correct+incorrect))*100}%</p>
                                         <p>Correct Answers: ${correct}</p>
                                         <p>Incorrect Answers: ${incorrect}</p>`);
                             $('#restart').show();
@@ -96,7 +152,7 @@ $(document).ready(function() {
         correct = 0;
         incorrect = 0;
         questionCount = 0;
-        $('#mainContent').text("Prepare yourself to try again.  Remember: you only have 20 seconds to answer each question!");
+        $('.questionStem').text("Prepare yourself to try again.  Remember: you only have 20 seconds to answer each question!");
         playGame();
     })
 })
